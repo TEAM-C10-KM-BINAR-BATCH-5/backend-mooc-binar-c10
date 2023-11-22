863500c689068ec904bb8095aaf390ca08dcc78e
rianwp <rianwp2015@gmail.com>
2023-11-22 17:05:28 +0700

    [adrian] - changing status to message, and adding success property in every endpoint

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,94 +1,102 @@
 const { Course, Module, Video } = require("../models")
 
 const createCourse = async (req, res) => {
-  const {
-    title,
-    category,
-    level,
-    courseType,
-    imageUrl,
-    trailerUrl,
-    rating,
-    instructor,
-    duration,
-    telegramLink,
-    moduleCount,
-    about,
-    objective,
-    price
-  } = req.body
-  try {
-    const newCourse = await Course.create({
-      title,
-      about,
-      objective,
-      category,
-      level,
-      courseType,
-      imageUrl,
-      trailerUrl,
-      rating,
-      instructor,
-      duration,
-      telegramLink,
-      moduleCount,
-      price
-    })
+	const {
+		title,
+		category,
+		level,
+		courseType,
+		imageUrl,
+		trailerUrl,
+		rating,
+		instructor,
+		duration,
+		telegramLink,
+		moduleCount,
+		about,
+		objective,
+		price,
+	} = req.body
+	try {
+		const newCourse = await Course.create({
+			title,
+			about,
+			objective,
+			category,
+			level,
+			courseType,
+			imageUrl,
+			trailerUrl,
+			rating,
+			instructor,
+			duration,
+			telegramLink,
+			moduleCount,
+			price,
+		})
 
-    res.status(200).json({
-      status: "Success, create course",
-      data: {
-        newCourse
-      }
-    })
-  } catch (error) {
-    res.status(500).json({
-      status: "Error",
-      message: error.message
-    })
-    console.log(error.message)
-  }
+		res.status(200).json({
+			success: true,
+			message: "Success, create course",
+			data: {
+				newCourse,
+			},
+		})
+	} catch (error) {
+		res.status(500).json({
+      success: false,
+			message: error.message,
+		})
+		console.log(error.message)
+	}
 }
 
 const getCourses = async (req, res) => {
-  try {
-    const coursesData = await Course.findAll({
-      include: [
-        {
-          model: Module,
-          attributes: { exclude: ["createdAt", "updatedAt"] },
-          include: [{ model: Video, attributes: { exclude: ["createdAt", "updatedAt"] } }]
-        }
-      ]
-    })
-    res.status(200).json({
-      status: "Success, fetch",
-      courses: coursesData
-    })
-  } catch (error) {
-    res.status(500).json({
-      status: "Error",
-      message: error.message
-    })
-    console.log(error.message)
-  }
+	try {
+		const coursesData = await Course.findAll({
+			include: [
+				{
+					model: Module,
+					attributes: { exclude: ["createdAt", "updatedAt"] },
+					include: [
+						{
+							model: Video,
+							attributes: { exclude: ["createdAt", "updatedAt"] },
+						},
+					],
+				},
+			],
+		})
+		res.status(200).json({
+      success: true,
+			message: "Success, fetch",
+			courses: coursesData,
+		})
+	} catch (error) {
+		res.status(500).json({
+      success: false,
+			message: error.message,
+		})
+		console.log(error.message)
+	}
 }
 
 const deleteCourse = async (req, res) => {
-  const { id } = req.params
-  try {
-    await Course.destroy({ where: { id } })
-    res.status(200).json({
-      status: "Success, deleted",
-      data: null
-    })
-  } catch (error) {
-    res.status(500).json({
-      status: "Error",
-      message: error.message
-    })
-    console.log(error.message)
-  }
+	const { id } = req.params
+	try {
+		await Course.destroy({ where: { id } })
+		res.status(200).json({
+      success: true,
+			message: "Success, deleted",
+			data: null,
+		})
+	} catch (error) {
+		res.status(500).json({
+      success: false,
+			message: error.message,
+		})
+		console.log(error.message)
+	}
 }
 
 // const updateVideo = async(req,res)=>{
@@ -96,7 +104,7 @@ const deleteCourse = async (req, res) => {
 // }
 
 module.exports = {
-  createCourse,
-  getCourses,
-  deleteCourse
+	createCourse,
+	getCourses,
+	deleteCourse,
 }
--- controllers/moduleController.js
+++ controllers/moduleController.js
@@ -1,137 +1,149 @@
 const { Module, Video, Course } = require("../models")
 
 const createModule = async (req, res) => {
-  const { title, duration, courseId } = req.body
-  try {
-    let idCourse
-    if (courseId) {
-      const course = await Course.findOne({ where: { id: courseId } })
-      if (!course) {
-        return res.status(400).json({
-          status: `Bad request / cause course with id ${courseId} not found`
-        })
-      }
-      idCourse = course.id
-    }
-    const newModule = await Module.create({
-      title,
-      duration,
-      courseId: idCourse
-    })
+	const { title, duration, courseId } = req.body
+	try {
+		let idCourse
+		if (courseId) {
+			const course = await Course.findOne({ where: { id: courseId } })
+			if (!course) {
+				return res.status(400).json({
+					success: false,
+					message: `Bad request / cause course with id ${courseId} not found`,
+				})
+			}
+			idCourse = course.id
+		}
+		const newModule = await Module.create({
+			title,
+			duration,
+			courseId: idCourse,
+		})
 
-    res.status(200).json({
-      status: "Success, create module",
-      data: {
-        newModule
-      }
-    })
-  } catch (error) {
-    res.status(500).json({
-      status: "Error",
-      message: error.message
-    })
-    console.log(error.message)
-  }
+		res.status(200).json({
+			success: true,
+			message: "Success, create module",
+			data: {
+				newModule,
+			},
+		})
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			message: error.message,
+		})
+		console.log(error.message)
+	}
 }
 
 const getModules = async (req, res) => {
-  try {
-    const modules = await Module.findAll({
-      include: [{ model: Video, attributes: { exclude: ["createdAt", "updatedAt"] } }]
-    })
-    res.status(200).json({
-      status: "Success, fetch",
-      data: {
-        modules
-      }
-    })
-  } catch (error) {
-    res.status(500).json({
-      status: "Error",
-      message: error.message
-    })
-    console.log(error.message)
-  }
+	try {
+		const modules = await Module.findAll({
+			include: [
+				{ model: Video, attributes: { exclude: ["createdAt", "updatedAt"] } },
+			],
+		})
+		res.status(200).json({
+			success: true,
+			message: "Success, fetch",
+			data: {
+				modules,
+			},
+		})
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			message: error.message,
+		})
+		console.log(error.message)
+	}
 }
 
 const deleteModule = async (req, res) => {
-  const { id } = req.params
-  try {
-    await Module.destroy({ where: { id } })
-    res.status(200).json({
-      status: "Success, deleted",
-      data: null
-    })
-  } catch (error) {
-    res.status(500).json({
-      status: "Error",
-      message: error.message
-    })
-    console.log(error.message)
-  }
+	const { id } = req.params
+	try {
+		await Module.destroy({ where: { id } })
+		res.status(200).json({
+			success: true,
+			message: "Success, deleted",
+			data: null,
+		})
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			message: error.message,
+		})
+		console.log(error.message)
+	}
 }
 
 const updateModule = async (req, res) => {
-  const { id } = req.params
-  const { title, duration, courseId } = req.body
-  try {
-    let idCourse
-    if (courseId) {
-      const course = await Course.findOne({ where: { id: courseId } })
-      if (!course) {
-        return res.status(400).json({
-          status: `Bad request / cause course with id ${courseId} not found`
-        })
-      }
-      idCourse = course.id
-    }
-    const updatedModule = await Module.update(
-      {
-        title,
-        duration,
-        courseId: idCourse
-      },
-      { where: { id }, returning: true }
-    )
+	const { id } = req.params
+	const { title, duration, courseId } = req.body
+	try {
+		let idCourse
+		if (courseId) {
+			const course = await Course.findOne({ where: { id: courseId } })
+			if (!course) {
+				return res.status(400).json({
+					success: false,
+					message: `Bad request / cause course with id ${courseId} not found`,
+				})
+			}
+			idCourse = course.id
+		}
+		const updatedModule = await Module.update(
+			{
+				title,
+				duration,
+				courseId: idCourse,
+			},
+			{ where: { id }, returning: true }
+		)
 
-    res.status(200).json({
-      status: "Success, updated",
-      data: {
-        updatedModule
-      }
-    })
-  } catch (error) {
-    res.status(500).json({
-      status: "Error",
-      message: error.message
-    })
-    console.log(error.message)
-  }
+		res.status(200).json({
+			success: true,
+			message: "Success, updated",
+			data: {
+				updatedModule,
+			},
+		})
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			message: error.message,
+		})
+		console.log(error.message)
+	}
 }
 
 const getModule = async (req, res) => {
-  const { id } = req.params
-  try {
-    const module = await Module.findOne({ where: { id }, include: [{ model: Video }] })
-    res.status(200).json({
-      status: "Success, fetch",
-      data: {
-        module
-      }
-    })
-  } catch (error) {
-    res.status(500).json({
-      status: "Error",
-      message: error.message
-    })
-    console.log(error.message)
-  }
+	const { id } = req.params
+	try {
+		const module = await Module.findOne({
+			where: { id },
+			include: [{ model: Video }],
+		})
+		res.status(200).json({
+			success: true,
+			message: "Success, fetch",
+			data: {
+				module,
+			},
+		})
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			message: error.message,
+		})
+		console.log(error.message)
+	}
 }
 
 module.exports = {
-  createModule,
-  getModules,
-  deleteModule,
-  updateModule,
-  getModule
+	createModule,
+	getModules,
+	deleteModule,
+	updateModule,
+	getModule,
 }
