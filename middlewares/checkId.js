90eb9495588cf786823c8309592dee19d0dead81
Dimas-Mukhtar <95218465+Dimas-Mukhtar@users.noreply.github.com>
2023-11-23 16:58:22 +0700

    [dimas] - change status cod checkId

--- middlewares/checkId.js
+++ middlewares/checkId.js
@@ -1,17 +1,17 @@
-const ApiError = require("../utils/apiError");
+const ApiError = require("../utils/apiError")
 
 const checkId = (db) => {
   return async (req, res, next) => {
     try {
-      const find = await db.findByPk(req.params.id);
+      const find = await db.findByPk(req.params.id)
       if (!find) {
-        next(new ApiError(`id does not exist`, 400));
+        next(new ApiError(`id does not exist`, 404))
       }
-      next();
+      next()
     } catch (err) {
-      next(new ApiError(err.message, 500));
+      next(new ApiError(err.message, 500))
     }
-  };
-};
+  }
+}
 
-module.exports = checkId;
+module.exports = checkId
