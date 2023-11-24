43a2aa19ac532ac224667ffdfbe63a4c793b1dba
Arfiano Jordhy <arfianoj@gmail.com>
2023-11-25 00:54:23 +0700

    [jordhy] - add otp verification on register

--- server.js
+++ server.js
@@ -1,23 +1,35 @@
-require("dotenv").config()
-const express = require("express")
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
 
-const app = express()
-const PORT = process.env.PORT || 4000
-const ApiError = require("./utils/apiError")
-const errorHandler = require("./controllers/errorController")
-const router = require("./routes")
-const setHeaders = require("./middlewares/setHeaders")
+const app = express();
+app.use(
+  session({
+    secret: "your-secret-key",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 60000, // 60 seconds
+    },
+  })
+);
 
-app.use(express.json())
-app.use(setHeaders)
-app.use(router)
+const PORT = process.env.PORT || 4000;
+const ApiError = require("./utils/apiError");
+const errorHandler = require("./controllers/errorController");
+const router = require("./routes");
+const setHeaders = require("./middlewares/setHeaders");
+
+app.use(express.json());
+app.use(setHeaders);
+app.use(router);
 
 app.all("*", (req, res, next) => {
-	next(new ApiError(`Routes does not exist`, 404))
-})
+  next(new ApiError(`Routes does not exist`, 404));
+});
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(PORT, () => {
-	console.log(`Server is running on port : ${PORT}`)
-})
+  console.log(`Server is running on port : ${PORT}`);
+});
