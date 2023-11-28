555f9960c60b7b3abdd6144010b88bc06ff655b2
rianwp <rianwp2015@gmail.com>
2023-11-28 23:11:35 +0700

    [adrian] - adding development and production database name

--- config/config.js
+++ config/config.js
@@ -4,14 +4,14 @@ module.exports = {
 	development: {
 		username: process.env.DB_USERNAME,
 		password: process.env.DB_PASSWORD,
-		database: process.env.DB_NAME,
+		database: `${process.env.DB_NAME}_development`,
 		host: process.env.DB_HOST,
 		dialect: "postgres",
 	},
 	production: {
 		username: process.env.DB_USERNAME,
 		password: process.env.DB_PASSWORD,
-		database: process.env.DB_NAME,
+		database: `${process.env.DB_NAME}_production`,
 		host: process.env.DB_HOST,
 		dialect: "postgres",
 	},
