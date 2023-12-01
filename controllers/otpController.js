7667c2845eeb32c9f7f82c9a0fea544b13bddfac
Arfiano Jordhy <arfianoj@gmail.com>
2023-12-01 09:29:27 +0700

    [jordhy] - updated otp email design

--- controllers/otpController.js
+++ controllers/otpController.js
@@ -1,7 +1,7 @@
-const ApiError = require("../utils/apiError")
+const ApiError = require("../utils/apiError");
 
-const otpGenerator = require("otp-generator")
-const nodemailer = require("nodemailer")
+const otpGenerator = require("otp-generator");
+const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -10,37 +10,47 @@ const transporter = nodemailer.createTransport({
   secure: true,
   auth: {
     user: process.env.USER_EMAIL,
-    pass: process.env.APP_PASSWORD
-  }
-})
+    pass: process.env.APP_PASSWORD,
+  },
+});
 
 const createOtp = async (req, res, next) => {
+  const learnhubLogo = "";
   try {
     const otp = otpGenerator.generate(6, {
       upperCaseAlphabets: false,
       lowerCaseAlphabets: false,
-      specialChars: false
-    })
+      specialChars: false,
+    });
 
     await transporter.sendMail({
-      from: '"Learn Hub" <binar.team.c10@gmail.com>', // sender address
-      to: req.body.email, // list of receivers
-      subject: "Email Verification OTP", // Subject line
-      html: `<b>Your OTP is: ${otp}</b>` // html body
-    })
+      from: '"Learn Hub" binar.team.c10@gmail.com',
+      to: req.body.email,
+      subject: "Email Verification OTP",
+      html: `<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
+      <div style="margin:50px auto;width:70%;padding:20px 0">
+      <hr style="border:none;border-top:1px solid #eee" />
+        <p style="font-size:1.1em">Hi,</p>
+        <p>Thank you for using Learn Hub. Use the following OTP to complete your register process. OTP is valid for 60 seconds</p>
+        <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 8px;">${otp}</h2>
+        <p style="font-size:0.9em;">Learn Hub</p>
+        <hr style="border:none;border-top:1px solid #eee" />
+      </div>
+    </div>`,
+    });
 
-    req.session.otp = otp
+    req.session.otp = otp;
 
     return res.status(200).json({
       success: true,
       message: "Success, sent",
-      data: otp
-    })
+      data: otp,
+    });
   } catch (err) {
-    return next(new ApiError(err.message, 500))
+    return next(new ApiError(err.message, 500));
   }
-}
+};
 
 module.exports = {
-  createOtp
-}
+  createOtp,
+};
