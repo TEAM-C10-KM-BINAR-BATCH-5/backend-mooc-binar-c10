3830d52a7f9c2f25ba25de494060ea890572888c
Dimas-Mukhtar <95218465+Dimas-Mukhtar@users.noreply.github.com>
2023-12-05 16:06:01 +0700

    [dimas] - add test database config

--- config/config.js
+++ config/config.js
@@ -9,6 +9,14 @@ module.exports = {
     port: process.env.DB_PORT,
     dialect: "postgres"
   },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: `${process.env.DB_NAME}_test`,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres"
+  },
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
