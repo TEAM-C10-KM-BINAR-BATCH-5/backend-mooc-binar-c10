5c30f17f7ba7b971a4bed1f98f999e2053541b06
rianwp <rianwp2015@gmail.com>
2023-12-06 23:04:23 +0700

    [adrian] - fix error controller error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,6 +1,8 @@
 module.exports = (err, req, res) => {
   // eslint-disable-next-line no-param-reassign
   err.statusCode = err.statusCode || 500
+  // eslint-disable-next-line no-param-reassign, no-self-assign
+  err.message = err.message
 
   res.status(err.statusCode).json({
     success: false,
