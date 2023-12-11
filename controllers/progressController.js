e0e7f5962a0360359ca7da37c024506f6e10e245
rianwp <rianwp2015@gmail.com>
2023-12-11 15:48:04 +0700

    [adrian] - try debug update progress

--- controllers/progressController.js
+++ controllers/progressController.js
@@ -1,6 +1,6 @@
 // prettier-ignore
 const {
-  Video, UserVideo, Module, Course,
+  Video, UserVideo, Module,
 } = require('../models')
 const ApiError = require('../utils/apiError')
 
@@ -8,15 +8,20 @@ const updateProgress = async (req, res, next) => {
   const videoId = Number(req.params.id)
   try {
     const video = await Video.findByPk(videoId, {
-      include: [{ model: Module }],
+      include: [
+        {
+          model: Module,
+          attributes: ['courseId'],
+        },
+      ],
     })
     if (!video) {
       return next(new ApiError('id does not exist', 404))
     }
-    // await UserVideo.create({
-    //   userId: req.user.id,
-    //   courseId: video.Module.Course.id,
-    // })
+    await UserVideo.create({
+      userId: req.user.id,
+      courseId: video.Module.couseId,
+    })
     return res.status(200).json({
       success: true,
       message: 'Success update progress',
