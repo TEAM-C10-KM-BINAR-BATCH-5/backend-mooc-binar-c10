59fb0fbb41943465c8ac05becd49128e09c52c9f
Arfiano Jordhy <arfianoj@gmail.com>
2023-12-18 12:01:07 +0700

    [jordhy] update otp email message

--- controllers/otpController.js
+++ controllers/otpController.js
@@ -28,12 +28,12 @@ const createOtp = async (req, res, next) => {
     await transporter.sendMail({
       from: '"Learn Hub" binar.team.c10@gmail.com',
       to: req.body.email,
-      subject: 'Email Verification OTP',
+      subject: 'OTP Verifikasi Email',
       html: `<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
       <div style="margin:50px auto;width:70%;padding:20px 0">
       <hr style="border:none;border-top:1px solid #eee" />
         <p style="font-size:1.1em">Hi,</p>
-        <p>Thank you for using Learn Hub. Use the following OTP to complete your register process. OTP is valid for 60 seconds</p>
+        <p>Terima kasih telah menggunakan Learn Hub. Gunakan OTP berikut untuk menyelesaikan proses pendaftaran Anda. OTP berlaku selama 5 menit</p>
         <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 8px;">${otp}</h2>
         <p style="font-size:0.9em;">Learn Hub</p>
         <hr style="border:none;border-top:1px solid #eee" />
