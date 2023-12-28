bd69b0e2132c002bd22b56122fb3685c2981e736
rianwp <rianwp2015@gmail.com>
2023-12-28 19:42:15 +0700

    [adrian] - fix category all

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -78,7 +78,7 @@ const getCourses = async (req, res, next) => {
   const popularitySort = req.query.popularity
   const whereClause = {}
 
-  if (categoryIds.length > 0 || !categoryIds.includes('C-0ALL')) {
+  if (categoryIds.length > 0 && !categoryIds.includes('C-0ALL')) {
     whereClause.categoryId = {
       [Op.in]: categoryIds,
     }
